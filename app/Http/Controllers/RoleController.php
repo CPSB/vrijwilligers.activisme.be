5b022b0a56351eea5d295738d365060262215513
Tim Joosten <Tjoosten3@gmail.com>
2017-06-14 23:01:38 +0200

    Add final touches acl

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Role;
+use App\Permission;
+use App\Traits\Authorizable;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
+    use Authorizable;
+
     /**
      * Display a listing of the resource.
      *
@@ -14,17 +18,10 @@ class RoleController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $roles       = Role::all(); 
+        $permissions = Permission::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return view('role.index', compact('roles', 'permissions'));
     }
 
     /**
@@ -35,29 +32,13 @@ class RoleController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $this->validate($request, ['name' => 'required|unique:roles']); 
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Role  $role
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Role $role)
-    {
-        //
-    }
+        if (Role::create($request->only('name'))) {
+            flash('Role added.');
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Role  $role
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Role $role)
-    {
-        //
+        return redirect()->back();
     }
 
     /**
@@ -69,17 +50,19 @@ class RoleController extends Controller
      */
     public function update(Request $request, Role $role)
     {
-        //
-    }
+        if ($role = Role::findOrFail($id)) {
+            if ($role->name === 'Admin') {  // admin role has everything
+                $role->syncPermissions(Permission::all());
+                return redirect()->route('roles.index');
+            }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Role  $role
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Role $role)
-    {
-        //
+            $permissions = $request->get('permissions', []);
+            $role->syncPermissions($permissions);
+            flash( $role->name . ' permissions has been updated.');
+        } else {
+            flash()->error( 'Role with id '. $id .' note found.');
+        }
+
+        return redirect()->route('roles.index');
     }
 }
