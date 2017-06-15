676bd1b442e80138d6e2491310d448022167097c
Tim Joosten <Tjoosten3@gmail.com>
2017-06-16 00:11:56 +0200

    Implement notifications to the navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
                 <div class="container">
                     <div class="navbar-header">
 
-                        <!-- Collapsed Hamburger -->
+                        {{-- Collapsed Hamburger --}}
                         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                             <span class="sr-only">Toggle Navigation</span>
                             <span class="icon-bar"></span>
@@ -27,14 +27,14 @@
                             <span class="icon-bar"></span>
                         </button>
 
-                        <!-- Branding Image -->
+                        {{-- Branding Image --}}
                         <a class="navbar-brand" href="{{ url('/') }}">
                             {{ config('app.name', 'Laravel') }}
                         </a>
                     </div>
 
                     <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                        <!-- Left Side Of Navbar -->
+                        {{-- Left Side Of Navbar --}}
                         <ul class="nav navbar-nav">
                             @if (Auth::check())
                                 @can('view_users')
@@ -66,13 +66,18 @@
                             @endif
                         </ul>
 
-                        <!-- Right Side Of Navbar -->
+                        {{-- Right Side Of Navbar --}}
                         <ul class="nav navbar-nav navbar-right">
-                            <!-- Authentication Links -->
+                            {{-- Authentication Links --}}
                             @if (Auth::guest())
-                                <li><a href="{{ route('login') }}">Login</a></li>
-                                <li><a href="{{ route('register') }}">Register</a></li>
+                                <li><a href="{{ route('login') }}"><span class="fa fa-sign-in" aria-hidden="true"></span> Login</a></li>
                             @else
+                                <li>
+                                    <a href="">
+                                        <span class="fa fa-bell-o" aria-hidden="true"></span>
+                                        <span class="badge">0</span>
+                                    </a>
+                                </li>
                                 <li class="dropdown">
                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                         {{ Auth::user()->name }} <span class="caret"></span>
@@ -107,7 +112,7 @@
             </div>
         </div>
 
-        <!-- Scripts -->
+        {{-- Scripts --}}
         <script src="{{ asset('js/app.js') }}"></script>
 
         @stack('scripts')
