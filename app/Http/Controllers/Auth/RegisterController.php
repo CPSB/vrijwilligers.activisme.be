0a4f9cc2078f3fdbbf1f4d70012c1f28db3c1cdc
Tjoosten <Tjoosten3@gmail.com>
2017-06-20 12:30:57 +0200

    Implement User role on registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -72,10 +72,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+        
+        $user->assignRole('User');
+        return $user;
     }
 }
